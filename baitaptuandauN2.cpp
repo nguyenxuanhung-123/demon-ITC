a853a7ce0bc071b421ef0b6e74ad42d47c59847f
nguyenxuanhung-123 <nxhung.dhti15a21hn@sv.uneti.edu.vn.com>
2022-03-15 22:03:28 +0700

    baitaptuan1

---
+++ baitaptuandauN2.cpp
@@ -0,0 +1,95 @@
+#include<iostream.h>
+int main(){
+	//If else 
+	/*
+	cout<<"-------bai1------------" 
+	int n;
+	cout<<"nhap vao thu trong tuan:"<<endl;
+	cin>>n;
+	if(n==1){
+		cout<<"sunday"<<endl; 
+	} 
+	else if(n==2){
+		cout<<"monday"<<endl; 
+	} 
+	else if(n==3){
+		cout<<"tuesday"<<endl; 
+	} 
+	else if(n==4){
+		cout<<"wednesday"<<endl; 
+	} 
+	else if(n==5){
+		cout<<"thursday"<<endl; 
+	}
+	else if(n==6){
+		cout<<"friday"<<endl; 
+	} 
+	else if(n==7){
+		cout<<"saturday"<<endl; 
+	} 
+	else{
+		cout<<"thu khong hop le xin moi nhap lai"<<endl; 
+	} 
+	*/
+	
+	/*
+	cout<<"=========bai2=========="<<endl;
+	float sodien,tiendien;
+	int bac1=2000,bac2=2500,bac3=3000,bac4=5000;
+	cout<<"nhap vao so kw dien tieu thu:"<<endl;
+	cin>>sodien;
+	if(sodien<0){
+		cout<<"vui long nhap lai so dien di ban oi"<<endl; 
+	} else if(sodien<=100){
+		tiendien=sodien*bac1; 
+		cout<<"so tien dien trong muc nay la:"<<" "<<tiendien<<endl; 
+	} else if(sodien<=200){
+		tiendien= 100*bac1 + (sodien-100)*bac2;
+		cout<<"so tien dien trong muc nay la:"<<" "<<tiendien<<endl;
+		 
+	} else if(sodien<=300){
+		tiendien=100*bac1 +100*bac2 +(sodien-200)*bac3;
+		cout<<"so tien dien trong muc nay la:"<<tiendien<<endl; 
+	} 
+	else if(sodien>300){
+		tiendien=100*bac1 + 100*bac2 +100*bac3 +(sodien-300)*bac4;
+		cout<<"so tien dien trong muc nay la:"<<tiendien<<endl; 
+	} 
+		
+	 */
+	  
+	  //vong lap for
+	  /*
+	   cout<<"===========bai1============="<<endl;
+	   int n,p=1;
+	 
+	   do{
+	   	
+	   cout<<"nhap vao n:"<<endl;
+	   
+	     cin>>n; 
+   		
+		   
+   	}while(n<=0);
+   	 
+   	for(int i=2;i<=n;i++){
+	   	p=p*2*i; 
+	   }
+	   cout<<"tich cua chuong chinh la:"<<p<<endl; 
+	*/
+	
+	/*
+	cout<<"==========bai2=========="<<endl;
+	int n;
+	cout<<"nhap vao n:"<<endl;
+	cin>>n;
+	for(int i=n;i>=1;i--){
+		for(int j=i;j>=1;j--){
+			 cout<<"*";
+		}
+		cout<<endl; 
+	 	 
+	} 
+	*/ 
+	return 0; 
+} 
